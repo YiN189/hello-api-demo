a7ff294be4624483183a203074bd52557b6cb351
Nyi Phyo Kyaw <me@npkk.dev>
2026-01-23 10:53:11 +0700

    Add CORS support

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,9 +1,19 @@
+import corsHeaders from "@/lib/cors";
 import { NextResponse } from "next/server";
+
+export async function OPTIONS(req) {
+  return new Response(null, {
+    status: 200,
+    headers: corsHeaders,
+  });
+}
 
 export async function GET() {
   const message = {
     message: "hello world"
   };
 
-  return NextResponse.json(message);
+  return NextResponse.json(message, {
+    headers: corsHeaders,
+  });
 }
